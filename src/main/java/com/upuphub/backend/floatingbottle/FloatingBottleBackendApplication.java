8aad50513802ec9948b885a5f19111a72b2cfee5
LeoWang <LeoWang@Inslabs.org>
2020-07-06 16:27:57 +0800

    [INIT]INIT漂流瓶后端项目

---
+++ src/main/java/com/upuphub/backend/floatingbottle/FloatingBottleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.upuphub.backend.floatingbottle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloatingBottleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloatingBottleBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upuphub/backend/floatingbottle/FloatingBottleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upuphub.backend.floatingbottle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloatingBottleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
